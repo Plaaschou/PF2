37856e335bc32df73266868956a78ef424c901ca
Seb <42071127+Plaaschou@users.noreply.github.com>
2020-03-12 11:55:52 +0100

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,3 +1,13 @@
+// Define function using repeat-while loop
+func rw_sum(n: Int) -> Int{
+  var total = 0, cnt = 1                // Declare the sum to return, cnt to count itterations
+  repeat {
+    total += cnt                        // Sums up 
+    cnt += 1                            // Increase cnt on each itteration
+  } while cnt <= n                      // Repeats until cnt exceeds n
+  return total 
+}
+
 // Define function using for loop
 func for_sum(n : Int) -> Int{
   var total = 0                         // Declare the value to return at the end
@@ -17,15 +27,6 @@ func while_sum(n : Int) -> Int{
   return total
 }
 
-// Define function using repeat-while loop
-func rw_sum(n: Int) -> Int{
-  var total = 0, cnt = 1                // Declare the sum to return, cnt to count itterations
-  repeat {
-    total += cnt                        // Sums up 
-    cnt += 1                            // Increase cnt on each itteration
-  } while cnt <= n                      // Repeats until cnt exceeds n
-  return total 
-}
 
 // Define function using direct calculation
 func direct_sum(n : Int) -> Int{       // Using double to prevent integer division
